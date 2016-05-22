7ee0bbb1fa829d191abc74160d713109ddca62e7
SNYdev <yanis.sahnoune@supinternet.fr>
2016-05-22 20:47:29 +0200

    add js scripts

---
+++ web/js/script.js
@@ -0,0 +1,31 @@
+jQuery(window).load(function(){
+
+  // $('#btn-dislike').click(function(){
+  //   valDislike++;
+  // })
+
+
+  //
+  //
+  // dislike.click(function(){
+  //
+  // });
+  //
+  // like.click(function(){
+  //
+  // });
+  //
+  //
+  // $.ajax({
+  //     url: "http://www.exercice.com/2-annee/Symfony-VDM/web/app_dev.php/bestvdm",
+  //     type: 'POST',
+  //     success : function(data, status){
+  //         console.log(data);
+  //     },
+  //     error : function(result, status, error){
+  //         console.log(result + status + error);
+  //         console.log("error");
+  //     }
+  // }); // Fin ajax
+
+});
